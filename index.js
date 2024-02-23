18becc7c03f34395a18fd59a941f09c9984d65a8
kayasit chareetsatien <kayasitjareetsatian@gmail.com>
2024-02-23 22:46:32 +0700

    .

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(express.json())
 console.log("Start\n\n\n\n")
 
-const dburl = "postgres://webadmin:GKAtod14125@node56582-env-8359299.proen.app.ruk-com.cloud/Books"
-    
+
+const dburl = "postgres://webadmin:GKAtod14125@node56582-env-8359299.proen.app.ruk-com.cloud:11881/Books" // delete port 11344 if wanna run on rukcom
 const sequelize = new Sequelize(dburl)
 
 
